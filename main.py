bc6d2aae8f2568f80185ab225025f969f91882ca
吴子豪 <vortezwohl@proton.me>
2025-08-02 23:32:33 +0800

    实现训练脚本

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ner-modeling!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ util.py
@@ -0,0 +1,7 @@
+import torch
+
+NUM_CLASSES = 9
+
+
+def one_hot(num: int) -> torch.Tensor:
+    return torch.nn.functional.one_hot(torch.tensor(num), num_classes=NUM_CLASSES)
